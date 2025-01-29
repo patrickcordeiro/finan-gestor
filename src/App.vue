d0fa8163a62965f701741ec779d22c7669d750dd
patrickcordeiro <patrick.cordeiro30@gmail.com>
2025-01-29 20:44:35 -0300

    refactor: Cleaning code

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import HeaderComponent from './components/HeaderComponent.vue'
+import AddTransactionForm from './components/AddTransactionForm.vue'
+</script>
+
+<template>
+  <HeaderComponent />
+  <main>
+    <div>
+      <AddTransactionForm />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px 0;
+
+  div {
+    width: 80%;
+  }
+}
+</style>
