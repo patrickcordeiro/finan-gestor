e0ff4eeef7b1c249d5058626711da007f801d403
patrickcordeiro <patrick.cordeiro30@gmail.com>
2025-01-29 20:45:01 -0300

    feat: Creating header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <div>
+      <h1>Finan Gestor</h1>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style>
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100px;
+  box-shadow: 0px 10px 32px -12px rgba(0, 0, 0, 0.25);
+  background-color: rgb(248, 249, 250);
+
+  div {
+    width: 80%;
+  }
+}
+</style>
